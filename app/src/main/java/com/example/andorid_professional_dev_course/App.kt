80ee76e7f3b3d94dfe403db1caf0b0b40233b4a1
Gleb <gleb.s.smirnov@gmail.com>
2022-06-13 14:10:35 +0400

    работаю над архитектурой и ui код в работе

---
+++ app/src/main/java/com/example/andorid_professional_dev_course/App.kt
@@ -0,0 +1,6 @@
+package com.example.andorid_professional_dev_course
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/example/andorid_professional_dev_course/domain/Contracts.kt
@@ -0,0 +1,7 @@
+package com.example.andorid_professional_dev_course.domain
+
+interface Contracts {
+    interface MainScreenContracts{
+        fun showData()//todo
+    }
+}
