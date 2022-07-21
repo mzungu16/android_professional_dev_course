43da4f578789f4eb06666c9be666be44108c0109
Gleb <gleb.s.smirnov@gmail.com>
2022-07-21 10:30:51 +0400

    добавил самые простые тесты

--- app/src/test/java/com/example/andorid_professional_dev_course/ExampleUnitTest.kt
+++ app/src/test/java/com/example/andorid_professional_dev_course/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.andorid_professional_dev_course
 
+import com.example.andorid_professional_dev_course.ui.MainActivity
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +11,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun string_isNotEmpty() {
+        val string = "string"
+        assertTrue(string.isNotEmpty())
+    }
+
+    @Test
+    fun string_isEquals() {
+        val string = "Hello World"
+        assertEquals("Hello World", string)
+    }
+
+    @Test
+    fun number_isInt(){
+        val number = 1
+        assertNotEquals(4,number + 4)
     }
 }
